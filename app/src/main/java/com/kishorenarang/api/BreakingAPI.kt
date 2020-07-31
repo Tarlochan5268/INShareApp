0f369f46c49d84e42cbc22711469fba34683b0e6
Kishore Narang <narangkishore98@gmail.com>
2020-07-31 16:27:59 -0400

    [Kishore] - Starting API Services for News.

---
+++ app/src/main/java/com/kishorenarang/api/BreakingAPI.kt
@@ -0,0 +1,72 @@
+package com.kishorenarang.api
+
+import android.util.Log
+
+class BreakingAPI
+{
+
+    private  val TAG = "News"
+    private constructor()
+    {
+
+    }
+
+
+
+    companion object {
+
+        private var obj:BreakingAPI? = null;
+
+
+        fun create() : BreakingAPI?
+        {
+            if(obj==null)
+            {
+                obj = BreakingAPI();
+
+            }
+            return obj;
+        }
+
+
+    }
+
+    fun call(url:String):String
+    {
+        var returner = "";
+        Log.d("KISHORE","CALL METHOD COLLED")
+        Thread(Runnable(){
+
+            val client = OkHttpClient()
+
+            val request = Request.Builder().url(url).build()
+            Log.d("KISHORE","INSIDE THREAD")
+
+            client.newCall(request).enqueue(object : Callback{
+
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.d("KISHORE","Failed");
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+
+                    Log.d(TAG, "onResponse: Receeived."+response.body!!.string());
+                    returner = response.body!!.string()
+
+
+                }
+            });
+
+        }).start()
+
+
+
+
+        return returner;
+
+
+    }
+
+}
