e412b2957a038d6c9abf551f5cd4c0c8382749a6
Kishore Narang <narangkishore98@gmail.com>
2020-08-04 12:40:35 -0400

    [Kishore] - Chips created selection de-selection completed

--- app/src/main/java/com/tarlochan/inshareapp/NewsFragment.kt
+++ app/src/main/java/com/tarlochan/inshareapp/NewsFragment.kt
@@ -1,10 +1,18 @@
 package com.tarlochan.inshareapp
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import kotlinx.android.synthetic.main.chip_category.*
+import kotlinx.android.synthetic.main.fragment_news.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +24,17 @@ private const val ARG_PARAM2 = "param2"
  * Use the [NewsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NewsFragment : Fragment() {
+class NewsFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
+    private  val TAG = "NewsFragment"
+
+    var chipGroup:ChipGroup? = null
+
+    val categories:Array<String> = arrayOf("Technology","Politics","Sports","Entertainment","Business","General","Science")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +42,12 @@ class NewsFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
+
+
+
+
     }
 
     override fun onCreateView(
@@ -35,6 +56,31 @@ class NewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false)
+
+
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d(TAG, "onViewCreated: "+filter_chips)
+
+        var id:Int = 0
+        for(category in categories)
+        {
+
+            val c = Chip(context)
+            c.id = ++id
+            c.text = category
+            c.chipBackgroundColor = getColorStateList(requireContext(), R.color.colorChip)
+
+            c.setOnClickListener(this);
+            filter_chips.addView(c)
+
+        }
+
     }
 
     companion object {
@@ -55,5 +101,32 @@ class NewsFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(p0: View?) {
+
+
+        val chipClicked = p0 as Chip
+
+
+        if(chipClicked.isSelected)
+        {
+            chipClicked.chipIcon = null
+            chipClicked.isSelected = false
+chipClicked.chipBackgroundColor = getColorStateList(requireContext(), R.color.colorChip)
+            chipClicked.setTextColor(Color.BLACK)
+        }
+        else
+        {
+            chipClicked.isSelected = true
+
+            chipClicked.chipIcon = resources.getDrawable(R.drawable.tick_icon,null)
+           chipClicked.chipBackgroundColor = getColorStateList(requireContext(), R.color.colorPrimary)
+            chipClicked.setTextColor(Color.WHITE)
+
+        }
+
+        Log.d(TAG, "onClick: "+chipClicked.text)
+
     }
 }
