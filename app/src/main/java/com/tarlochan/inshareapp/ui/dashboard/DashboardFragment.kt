485dce72ec8b6e1a456ce8cec3ca0f3ab3c45208
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-08-05 17:50:53 -0400

    DashBoard Fragment Code added to go to send receive screen

--- app/src/main/java/com/tarlochan/inshareapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/tarlochan/inshareapp/ui/dashboard/DashboardFragment.kt
@@ -1,12 +1,21 @@
 package com.tarlochan.inshareapp.ui.dashboard
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProviders
 import com.tarlochan.inshareapp.R
+import com.tarlochan.inshareapp.ReceiveFragment
+import com.tarlochan.inshareapp.SendFragment
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
@@ -20,8 +29,28 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        val btnSend: Button =  root.findViewById<Button>(R.id.btnSend)
+        btnSend.isActivated = false
+        btnSend.setOnClickListener{
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, SendFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+        val btnReceive: Button =  root.findViewById<Button>(R.id.btnReceive)
+        btnReceive.isActivated = false
+        btnReceive.setOnClickListener{
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, ReceiveFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+
         return root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
